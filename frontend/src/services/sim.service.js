4528c5282f0122ffa9d4aeef1a8026a62e768f6d
Brun02K23 <bvirinni@gmail.com>
2025-02-14 14:15:06 -0300

    ANDA AL 100% EL BACKEND

--- frontend/src/services/sim.service.js
+++ frontend/src/services/sim.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const simular = async () => {
     try {
-        const response = await axios.post('http://localhost:4000/final/simular');
+        const response = await axios.post('http://localhost:4000/final/simular', { cant_iteraciones_a_mostrar: 5000, dia_inicio_muestra: 2, hora_inicio_muestra: 0 });
         return response.data;
     } catch (error) {
         return error.response.data;
